622563b0ab34101516e476001529d5eeae8edd23
magid <magidmroueh@gmail.com>
2020-03-30 14:49:09 +0300

    fix: app module and use npm

--- src/shared/decorators/decorators.ts
+++ src/shared/decorators/decorators.ts
@@ -3,14 +3,8 @@ import { Response } from 'express';
 import { User } from '../../../generated/prisma-client';
 import { GqlExecutionContext } from '@nestjs/graphql';
 
-//TODO: check why this is not working.
-// export const ResGql = createParamDecorator(
-//   (data, [root, args, ctx, info]): Response => ctx.res,
-// );
-
 export const ResGql = createParamDecorator(
-  (data: unknown, ctx: ExecutionContext) =>
-    GqlExecutionContext.create(ctx).getContext().res,
+  (data, [root, args, ctx, info]): Response => ctx.res,
 );
 
 export const GqlUser = createParamDecorator(
